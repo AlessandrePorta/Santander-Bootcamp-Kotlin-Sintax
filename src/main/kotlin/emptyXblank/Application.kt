3334f97976ea77610c07070b948622ec2efe229f
Alessandre <ale_junior97@hotmail.com>
2021-06-29 17:11:26 -0300

    Blank X Empty

---
+++ src/main/kotlin/emptyXblank/Application.kt
@@ -0,0 +1,18 @@
+package emptyXblank
+
+fun main(){
+
+    val s = ""
+
+    //true
+    println(s.isEmpty()) //empty == a string apenas é empty se o tamanho for 0
+
+    false
+    println(s.isBlank()) //blank == quando há qualquer alteração ela é blank mesmo que seja apenas espaçamento
+
+    val a = "     "
+    //false
+    println(a.isEmpty())
+    //true
+    println(a.isBlank())
+}
