dd67184ca48e3cbeab478a5f5cd8cb750d86de27
Alessandre Porta <ale_junior97@hotmail.com>
2021-06-29 20:30:48 -0300

    Funcoes de Ordem Superior finalizado

--- src/main/kotlin/estudos/funcoesDeOrdemSuperior/FuncoesDeOrdemSuperior.kt
+++ src/main/kotlin/estudos/funcoesDeOrdemSuperior/FuncoesDeOrdemSuperior.kt
@@ -4,9 +4,15 @@ fun main(){
 
     val x:Int
     val z:Int
-    z = calculate(34, 90,::sum)
-    println(z)
+    z = calculate(34, 90,){a,b ->
+        println("Vou calcular! $a * $b")
+        (a*b)
+    }
+    println("Resultado: $z")
 }
+/*
+
+Operacoes caso feitos a mão
 
 fun sum(a1:Int, a2:Int) = a1.plus(a2)
 
@@ -15,6 +21,8 @@ fun sub(a1:Int, a2:Int) = a1.minus(a2)
 fun mult(a1:Int, a2:Int) = a1.times(a2)
 
 fun div(a1:Int, a2:Int) = a1.div(a2)
+
+*/
 
 fun calculate(n1:Int, n2:Int, operation:(Int,Int)->Int):Int{
     val result = operation(n1,n2)
