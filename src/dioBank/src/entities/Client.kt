b5ab6f7e0d9a4679ed06b73cff1c2c5a202db48c
Alessandre Porta <ale_junior97@hotmail.com>
2021-06-30 17:49:29 -0300

    Alter class e inner class

--- src/dioBank/src/entities/Client.kt
+++ src/dioBank/src/entities/Client.kt
@@ -1,8 +1,19 @@
 package entities
 
+//alter class
 class Client {
     var nome: String = "Alessandre"
     var cpf : String = "123.123.123-20"
+
+    /*
+
+    //Para criar uma classe dentro de uma classe usa inner class
+
+    inner class Endereco{
+        var rua: String = "Rua teste, 123"
+    }
+
+     */
 }
 
 fun main(){
@@ -10,4 +21,5 @@ fun main(){
 
     println(alessandre.nome)
     println(alessandre.cpf)
+    //println(alessandre.Endereco().rua)  para acessar a inner classe
 }
