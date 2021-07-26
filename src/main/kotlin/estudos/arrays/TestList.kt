572b5609b8936bc078c4e7adb46e215ac364f813
Alessandre Porta <ale_junior97@hotmail.com>
2021-07-26 17:45:54 -0300

    Groupby

--- src/main/kotlin/estudos/arrays/TestList.kt
+++ src/main/kotlin/estudos/arrays/TestList.kt
@@ -1,9 +1,9 @@
 package estudos.arrays
 
 fun main() {
-    val Joao = Funcionario("Joao", 1000.0)
-    val Gabriel = Funcionario("Gabriel", 2000.0)
-    val Maria = Funcionario("Maria", 3000.0)
+    val Joao = Funcionario("Joao", 1000.0, "CLT")
+    val Gabriel = Funcionario("Gabriel", 2000.0, "PJ")
+    val Maria = Funcionario("Maria", 3000.0, "CLT")
 
     val funcionarios = listOf(Joao, Gabriel, Maria)
 
@@ -14,11 +14,15 @@ fun main() {
 
     println("----------------------")
     funcionarios.sortedBy { it.salario }.forEach { println(it) }
+
+    println("----------------------")
+    funcionarios.groupBy { it.tipoContratacao }.forEach { println(it) }
 }
 
 class Funcionario(
     val nome: String,
-    val salario: Double
+    val salario: Double,
+    val tipoContratacao: String
 ) {
     override fun toString(): String =
         """
