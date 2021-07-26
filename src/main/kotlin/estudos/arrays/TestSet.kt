d5a0115e0a213e9d8fa82332cf274291efd3fd54
Alessandre Porta <ale_junior97@hotmail.com>
2021-07-26 17:55:34 -0300

    substract, intersect

---
+++ src/main/kotlin/estudos/arrays/TestSet.kt
@@ -0,0 +1,23 @@
+package estudos.arrays
+
+fun main(){
+    val Joao = Funcionario("Joao", 1000.0, "CLT")
+    val Gabriel = Funcionario("Gabriel", 2000.0, "PJ")
+    val Maria = Funcionario("Maria", 3000.0, "CLT")
+
+    val funcionarios1 = setOf(Joao, Gabriel)
+    val funcionarios2 = setOf(Maria)
+
+    val resultUnion = funcionarios1.union(funcionarios2)
+
+    resultUnion.forEach { println(it) }
+
+    println("-------------------")
+    val funcionarios3 = setOf(Joao, Gabriel, Maria)
+    val resultSubtract = funcionarios3.subtract(funcionarios2)
+    resultSubtract.forEach { println(it) }
+
+    println("--------------------")
+    val resultIntersect = funcionarios3.intersect(funcionarios2)
+    resultIntersect.forEach { println(it) }
+}
