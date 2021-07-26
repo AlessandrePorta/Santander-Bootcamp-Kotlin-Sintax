9cb0fbdfed988bce5523f25ee21e5e7919e94d19
Alessandre Porta <ale_junior97@hotmail.com>
2021-07-26 17:33:11 -0300

    listOf

---
+++ src/main/kotlin/estudos/arrays/TestList.kt
@@ -0,0 +1,25 @@
+package estudos.arrays
+
+fun main() {
+    val Joao = Funcionario("Joao", 1000.0)
+    val Gabriel = Funcionario("Gabriel", 2000.0)
+    val Maria = Funcionario("Maria", 3000.0)
+
+    val funcionarios = listOf(Joao, Gabriel, Maria)
+
+    funcionarios.forEach { println(it) }
+
+    println("---------------------")
+    println(funcionarios.find{it.nome == "Maria"})
+}
+
+class Funcionario(
+    val nome: String,
+    val salario: Double
+) {
+    override fun toString(): String =
+        """
+            Nome: $nome
+            Salário: $salario
+        """.trimIndent()
+}
