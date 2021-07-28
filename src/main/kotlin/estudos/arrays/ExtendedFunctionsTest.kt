a816a2f30d7a2d7d8b96d8f83044d4d336a9668b
Alessandre Porta <ale_junior97@hotmail.com>
2021-07-27 21:53:44 -0300

    Extended functions

---
+++ src/main/kotlin/estudos/arrays/ExtendedFunctionsTest.kt
@@ -0,0 +1,17 @@
+package estudos.arrays
+
+import java.math.BigDecimal
+
+fun main() {
+    val salarios = arrayOf(
+        "2000".toBigDecimal(),
+        "1500".toBigDecimal(),
+        "4000".toBigDecimal()
+    )
+
+    println("---------------------")
+    println(salarios.somatoria())
+
+    println("---------------------")
+    println(salarios.media())
+}
