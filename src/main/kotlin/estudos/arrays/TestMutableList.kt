c7d3bb1919f9b02639cc431f59424ccb7194b273
Alessandre Porta <ale_junior97@hotmail.com>
2021-07-27 21:11:52 -0300

    Mutable list

---
+++ src/main/kotlin/estudos/arrays/TestMutableList.kt
@@ -0,0 +1,37 @@
+package estudos.arrays
+
+fun main(){
+    val Joao = Funcionario("Joao", 1000.0, "CLT")
+    val Gabriel = Funcionario("Gabriel", 2000.0, "PJ")
+    val Maria = Funcionario("Maria", 3000.0, "CLT")
+
+    val funcionarios = mutableListOf(Joao, Maria)
+    funcionarios.forEach { println(it) }
+
+    println("---------- List -------------")
+
+    funcionarios.add(Gabriel)
+    funcionarios.forEach { println(it) }
+
+    println("-----------------------")
+
+    funcionarios.remove(Joao)
+    funcionarios.forEach { println(it) }
+
+
+    println("----------- SET ------------")
+    val funcionarioSet = mutableSetOf(Joao)
+    funcionarioSet.forEach { println(it) }
+
+    println("-----------------------")
+
+    funcionarioSet.add(Gabriel)
+    funcionarioSet.add(Maria)
+    funcionarioSet.forEach { println(it) }
+
+    println("-----------------------")
+
+    funcionarioSet.remove(Maria)
+    funcionarioSet.forEach { println(it) }
+
+}
